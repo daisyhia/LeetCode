468a9c68ceeb8540a0631c50d44fe619526567e1
daisyhia <wangjh93@163.com>
2016-02-03 21:41:14 +0800

    update 10

--- 10.RegularExpressionMatching.cpp
+++ 10.RegularExpressionMatching.cpp
@@ -8,22 +8,17 @@ public:
     bool isMatch(const char* ps, const char* pp) {
         while(*pp) { 
             if(*pp=='*') return isMatch(ps, pp+1);
-            if(!*(pp+1)) { 
+            if(!*(pp+1)||*(pp+1)!='*') { 
                 if(!*ps) return false;
                 if(*pp!='.' && *ps!=*pp) return false;
-                return !*(ps+1);
-            }
-            if(*(pp+1)=='*') {
+                return isMatch(ps+1, pp+1);
+            } else {
                 if(isMatch(ps, pp+2)) return true;
                 while(*ps && (*ps==*pp||*pp=='.')) {
                     if(isMatch(++ps,pp+2)) return true;
                 }
                 return false;
-            } else {
-                if(!*ps) return false;
-                if(*pp!='.' && *ps!=*pp) return false;
-                return isMatch(ps+1, pp+1);
-            }
+            } 
         }
         return !*ps;
     }

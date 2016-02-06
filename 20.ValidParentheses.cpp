f1a795a709f5d5100cd04f9f0c6fd957fc23c618
daisyhia <wangjh93@163.com>
2016-02-06 12:32:09 +0800

    update 20
    use stack

--- 20.ValidParentheses.cpp
+++ 20.ValidParentheses.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         const char *p = s.c_str();
-        char *m = new char[100];
+        stack<char> sta;
         while(*p) {
             if(*p=='('||*p=='['||*p=='{') {
-                m++;
-                *m = *p;
-            } else if(*p==')') {
-                if(!*m||*m!='(') return false;
-                m--;
-            } else if(*p==']') {
-                if(!*m||*m!='[') return false;
-                m--;
-            } else if(*p=='}') {
-                if(!*m||*m!='{') return false;
-                m--;
+                sta.push(*p);
+            } else {
+                if(sta.empty()) return false;
+                if(*p==')') {if(sta.top()!='(') return false;}
+                else {if(!(*p-sta.top()==2)) return false;}
+                sta.pop();
             }
             p++;
         }
-        return !*m;
+        return sta.empty();
     }
 };
